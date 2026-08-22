dd7ab8f611a911d0b09f5c6d0f5df2718a681960
Selina-Hossam <selenaselmy55@gmail.com>
2026-08-22 17:20:16 +0300

    Create solution-task1.cpp

---
+++ solution-task1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Operations{
+public:
+static int a;
+static int b;
+public:
+static int add(){
+    return a+b;
+}
+};
+int Operations::a=0;
+int Operations::b=0;
+
+int main() 
+{
+    cout<<"Enter first number: ";
+    cin>>Operations::a;
+    cout<<"Enter first number: ";
+    cin>>Operations::b;
+    cout<<"The total is: "<<Operations::add();
+    return 0;
+}
